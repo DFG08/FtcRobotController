9ce36c49634a8755c84cb43b6f618e1eb8b5f4b0
StarB_ <uve0508@naver.com>
2024-09-28 16:54:21 +0900

    이동 속도 최적화

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
@@ -15,19 +15,12 @@ import java.util.Set;
 
 
 public class Movement extends OpMode {
-
-
-
-
-
     DcMotor lf;
     DcMotor rf;
     DcMotor lb;
     DcMotor rb;
-
-    Gamepad pad;
-
     DcMotor[] motors = new DcMotor[4];
+
 
 
     @Override
@@ -37,26 +30,31 @@ public class Movement extends OpMode {
         lb = hardwareMap.get(DcMotor.class,"lb");
         rb = hardwareMap.get(DcMotor.class,"rb");
         motors = new DcMotor[]{lf,rf,lb,rb};
+
     }
 
     @Override
     public void loop() {
-        move(0.7f,0.3f);
+        move(7,3);
     }
 
-    public void move(float moveSpeed, float rotateSpeed){
+    public void move(float mSpd, float rSpd){
 
-        if(moveSpeed + rotateSpeed > 1){
+        if(mSpd + rSpd > 1){
             //power 최대치 = 1, 1이상 구동 X, 따라서 최고 속도가 1 초과시 최고 속도를 1로 조정
             float moveTemp, rotateTemp;
-            moveTemp = moveSpeed /(moveSpeed + rotateSpeed);
-            rotateTemp = rotateSpeed / (moveSpeed + rotateSpeed);
-            moveSpeed = moveTemp;
-            rotateSpeed = rotateTemp;
+            moveTemp = mSpd /(mSpd + rSpd);
+            rotateTemp = rSpd / (mSpd + rSpd);
+            mSpd = moveTemp;
+            rSpd = rotateTemp;
         }
-        float x =gamepad1.left_stick_x * moveSpeed;
-        float y =-gamepad1.left_stick_y * moveSpeed;
-        float r =gamepad1.right_stick_x * rotateSpeed;
+        if(gamepad1.right_stick_x == 0){
+            mSpd = 1;
+        }
+
+        float x = gamepad1.left_stick_x * mSpd;
+        float y = -gamepad1.left_stick_y * mSpd;
+        float r = gamepad1.right_stick_x * rSpd;
         lf.setPower(x + y + r);
         rf.setPower(y - x - r);
         lb.setPower(y - x + r);
